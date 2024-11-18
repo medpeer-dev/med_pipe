82f6e8e5e8865d1fe2a7a04a8609e1345ece2a6d
mpg-taichi-sato <taichi.sato@medpeer.co.jp>
2024-11-18 15:46:53 +0900

    create pipeline.rb

--- lib/med_pipe.rb
+++ lib/med_pipe.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require "med_pipe/version"
 require "med_pipe/engine"
+require "med_pipe/pipeline"
 
 module MedPipe
   # Your code goes here...
---
+++ lib/med_pipe/pipeline.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MedPipe::Pipeline
+  def run
+    puts "MedPipe::Pipeline#run"
+    1
+  end
+end
