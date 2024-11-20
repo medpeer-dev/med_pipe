9d085ac4979b1ec12eb6d0153443e6b93ad61a86
mpg-taichi-sato <taichi.sato@medpeer.co.jp>
2024-11-21 08:54:17 +0900

    migrationを修正
    コメント付加、priority追加

--- db/migrate/20241118063336_create_med_pipe_pipeline_plans.rb
+++ db/migrate/20241118063336_create_med_pipe_pipeline_plans.rb
@@ -3,14 +3,16 @@
 class CreateMedPipePipelinePlans < ActiveRecord::Migration[7.2]
   def change
     create_table :med_pipe_pipeline_plans do |t|
-      t.string :name, null: false
-      t.string :output_unit, null: false
-      t.date :target_date
-      t.string :upload_to
+      t.string :name, null: false, comment: "パイプライン名"
+      t.integer :priority, null: false, default: 0, comment: "実行優先度"
       t.string :status, null: false
+      t.string :output_unit, null: false, comment: "実行単位. 日ごと、全て等"
+      t.date :target_date, comment: "実行対象日. output_unit が daily の場合に指定"
+      t.bigint :data_count
+      t.bigint :file_size
+      t.string :upload_to
       t.datetime :started_at
       t.datetime :finished_at
-      t.integer :file_size
 
       t.timestamps
     end
