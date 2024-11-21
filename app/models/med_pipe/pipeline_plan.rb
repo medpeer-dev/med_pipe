9bdf5b01e8edb5814ebeb9f1e6b4ff0897bba946
mpg-taichi-sato <taichi.sato@medpeer.co.jp>
2024-11-21 09:06:44 +0900

    コメント修正

--- app/models/med_pipe/pipeline_plan.rb
+++ app/models/med_pipe/pipeline_plan.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MedPipe::PipelinePlan < MedPipe::ApplicationRecord
-  # TODO: 定義の仕方を変える
+  # TODO: Rails6記法のため、Rails8に上げる際に定義の仕方を変える
   # https://zenn.dev/kanazawa/articles/8bc1fcbba3ef1d#enum%E3%81%AE%E5%AE%9A%E7%BE%A9%E6%96%B9%E6%B3%95%E3%81%8C%E5%A4%89%E3%82%8F%E3%82%8B
   enum output_unit: {
     daily: "daily",
