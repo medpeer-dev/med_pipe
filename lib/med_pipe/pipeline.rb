a39c168ddcfd5259f4d884f70ee67b961c2eb71f
mpg-taichi-sato <taichi.sato@medpeer.co.jp>
2024-11-21 09:59:16 +0900

    add pipeline
    インタフェースは Apache Beam の Pipeline を参考にしています
    https://qiita.com/malt03/items/26571eeccf850815c6ac#%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E3%82%B3%E3%83%BC%E3%83%89

--- lib/med_pipe/pipeline.rb
+++ lib/med_pipe/pipeline.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
 
+# 直列に繋いだtaskを順番に実行するクラス
 class MedPipe::Pipeline
-  def run
-    puts "MedPipe::Pipeline#run"
-    1
+  def initialize
+    @tasks = []
+  end
+
+  # @param task [Object] def call(context, prev_result, &block) を実装したクラス
+  def apply(task)
+    @tasks << task
+    self
+  end
+
+  # @param context [Hash] Stores data during pipeline execution
+  def run(context = {}) = run_task_recursive(context)
+  # 展開すると以下のようになる
+  # @tasks[0].call(context, nil) do |prev_result|
+  #   @tasks[1].call(context, prev_result) do |prev_result|
+  #     @tasks[2].call(context, prev_result) do |prev_result|
+  #       nil
+  #     end
+  #   end
+  # end
+
+  private
+
+  def run_task_recursive(context, prev_result = nil, task_index = 0)
+    return prev_result if task_index >= @tasks.size
+
+    @tasks[task_index]&.call(context, prev_result) do |result|
+      run_task_recursive(context, result, task_index + 1)
+    end
   end
 end
--- spec/lib/med_pipe/pipeline_spec.rb
+++ spec/lib/med_pipe/pipeline_spec.rb
@@ -4,9 +4,63 @@ require "rails_helper"
 
 RSpec.describe MedPipe::Pipeline do
   describe "#run" do
-    it "returns a positive number" do
-      pipeline = MedPipe::Pipeline.new
-      expect(pipeline.run).to be 1
+    let(:pipeline) { described_class.new }
+
+    let(:task_class) do
+      Class.new do
+        def initialize(num)
+          @num = num
+        end
+
+        def call(_context, prev_result)
+          if prev_result.nil?
+            yield [@num]
+          else
+            yield prev_result + [@num]
+          end
+        end
+      end
+    end
+
+    context "タスクを中断しない場合" do
+      before do
+        index = 0
+        3.times do
+          index += 1
+          pipeline.apply(task_class.new(index))
+        end
+      end
+
+      it "全て順番に実行する" do
+        expect(pipeline.run).to eq([1, 2, 3])
+      end
+    end
+
+    context "タスクを中断する場合" do
+      let(:stop_task_class) do
+        Class.new do
+          def call(_context, prev_result)
+            prev_result
+          end
+        end
+      end
+
+      it "中断した結果を返す" do
+        index = 0
+        2.times do
+          index += 1
+          pipeline.apply(task_class.new(index))
+        end
+
+        pipeline.apply(stop_task_class.new)
+
+        3.times do
+          index += 1
+          pipeline.apply(task_class.new(index))
+        end
+
+        expect(pipeline.run).to eq([1, 2])
+      end
     end
   end
 end
