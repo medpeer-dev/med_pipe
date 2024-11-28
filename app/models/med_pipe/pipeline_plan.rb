c90454859f010fad7b7acd4df97d297d8a54fa3c
mpg-taichi-sato <taichi.sato@medpeer.co.jp>
2024-11-28 17:41:13 +0900

    enumの書き方を新記法に更新

--- app/models/med_pipe/pipeline_plan.rb
+++ app/models/med_pipe/pipeline_plan.rb
@@ -9,18 +9,16 @@ class MedPipe::PipelinePlan < MedPipe::ApplicationRecord
   validates :output_unit, presence: true
   validates :status, presence: true
 
-  # TODO: Rails6記法のため、Rails8に上げる際に定義の仕方を変える
-  # https://zenn.dev/kanazawa/articles/8bc1fcbba3ef1d#enum%E3%81%AE%E5%AE%9A%E7%BE%A9%E6%96%B9%E6%B3%95%E3%81%8C%E5%A4%89%E3%82%8F%E3%82%8B
-  enum status: {
+  enum :status, {
     waiting: "waiting",
     enqueued: "enqueued",
     running: "running",
     finished: "finished",
     failed: "failed"
-  }, _prefix: true
+  }, prefix: true, default: :waiting
 
-  enum output_unit: {
+  enum :output_unit, {
     daily: "daily",
     all: "all"
-  }, _prefix: true
+  }, prefix: true
 end
